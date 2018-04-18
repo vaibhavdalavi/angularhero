b3fd0035e2498af08740f82f838c64ed446d1e9d
vaibhavdalavi <P7109887@PIDC.COM>
2018-04-18 17:08:38 +0530

    first commit

---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Vaib' },
+    { id: 2, name: 'Pradi' },
+    { id: 3, name: 'Geet' },
+    { id: 4, name: 'Sandy' }
+];
